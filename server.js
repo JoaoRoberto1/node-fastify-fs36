e275fc1055229becd9d688242236e42646de3e0c
joao roberto <joao.robertodof@gmail.com>
2025-01-09 21:48:47 -0300

    Adicionando erro 404, não achar o id

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 // 1 - Importando o Fastify
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 import { pasteis } from './db/db.pasteis.js';
 
-// 1.1 - Instanciando o objeto fastify o qual poderemos acessar os métodos http
+// 1.1 - Instanciando o objeto fastify
 const fastify = Fastify({
     logger: true
 });
 
-//2 - Declarando a rota
+// 2 - Declarando as rotas
+
+// Rota raiz
 fastify.get('/', (request, reply) => {
-    return { message: 'Oi! chamado a partir da raiz!' }
+    return { message: 'Oi! chamado a partir da raiz!' };
 });
 
+// Rota para adicionar um pastel
 fastify.post('/pastel', (request, reply) => {
     const pastel = request.body;
 
@@ -35,32 +38,33 @@ fastify.post('/pastel', (request, reply) => {
     };
 });
 
+// Rota para retornar todos os pasteis
 fastify.get('/pasteis', (request, reply) => {
     return {
         data: pasteis,
         qtd: pasteis.length,
         message: 'Retornou todos os pastéis!'
-    }
+    };
 });
 
+// Rota para retornar um pastel pelo ID
 fastify.get('/pastel/:id', (request, reply) => {
-    //console.log('Request', request.params.id);
     const id = parseInt(request.params.id);
-    const produtosBd = pasteis.find(p => p.id === id);
+    const pastel = pasteis.find(p => p.id === id);
+
+    if (!pastel) {
+        // Tratamento para ID não encontrado
+        return reply.status(404).send({
+            error: 'Not Found',
+            message: `Nenhum pastel encontrado com o ID: ${id}`
+        });
+    }
 
     return {
-        data: produtosBd || null,
-        qtd: produtosBd ? 1 : 0,
-        message: produtosBd ? `Retornou o pastel com o id: ${id}` : `Não encontrado pastel com o id: ${id}`
-    }
-})
+        data: pastel,
+        message: `Retornou o pastel com o ID: ${id}`
+    };
+});
 
 // 3 - Iniciando o servidor
 fastify.listen({ port: 3000 });
-
-
-/* 4 - Executando o server.js para perceber as mudanças usar a flag --watch
- node --watch -- server.js */
-
-
-//const { nome, valor, tamanho, viagem, imagem } = request.body;
